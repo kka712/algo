57610245099467a182ae1098c559c53844576146
kka712 <rlska213@naver.com>
2025-02-14 17:20:26 +0900

    algo

---
+++ 짝수홀수개수/sol.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = []
+
+    for num in range(n+1, n):
+        answer += i
+        
+            
+  
+
+    return answer
+
+print(solution([1, 2, 3, 4, 5])) # =>[2, 3]
+print(solution([1, 3, 5, 7])) # =>[0, 4]

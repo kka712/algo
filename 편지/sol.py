0b59b9a1e39c51b081bbf292a9b7a2bbd6ea22d6
kka712 <rlska213@naver.com>
2025-03-14 17:05:39 +0900

    update

--- 편지/sol.py
+++ 편지/sol.py
@@ -1,10 +1,6 @@
 def solution(message):
-    answer = 0
-
-    
-
+    answer = len(message) * 2
     return answer
 
-
-print(solution(["happy birthday!"])) # => 30
-print(solution(["I love you~"])) # => 22
+print(solution("happy birthday!")) # => 30
+print(solution("I love you~")) # => 22
---
+++ 편지/sol2.py
@@ -0,0 +1,5 @@
+def solution(message):
+    return len(message)<<1
+
+print(solution(["happy birthday!"])) # => 30
+print(solution(["I love you~"])) # => 22
